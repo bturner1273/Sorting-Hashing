f0c2df0585822a2af4418300aab1f8d7b90708d7
bturner1273 <bturner1273@gmail.com>
2018-04-16 16:07:00 -0400

    quickSort now working for large numbers

--- driver.cpp
+++ driver.cpp
@@ -3,42 +3,42 @@
 using namespace std;
 
 
-void printArray(int* arr, int n);
-int* initRandomArray();
-int* copyArray(int* arr);
-void heapSort(int arr[], int n);
-void heapify(int arr[], int n, int i);
-int partition(int arr[], int l, int h);
-void quickSort(int arr[], int l, int h);
+void printArray(long long* arr, long long n);
+long long* initRandomArray();
+long long* copyArray(long long* arr);
+void heapSort(long long arr[], long long n);
+void heapify(long long arr[], long long n, long long i);
+int partition(long long arr[], int l, int h);
+void quickSort(long long arr[], int l, int h);
 int size  = 5000;
 
 
 int main(){
 
 
+
 //TESTING  
-    // int* a = initRandomArray();
-    // int n = sizeof(a)/sizeof(a[0]);
-    // quickSort(a,0,n-1);
-    
-    // printArray(a, 10);
+    long long* a = initRandomArray();
+    quickSort(a,0,size-1);
+    //heapSort(a,size);
+    printArray(a,size);
 
 
 
 
     // heapSort test
 
-    // int arr[] = {12, 11, 13, 5, 6, 7};
-    // int n = sizeof(arr)/sizeof(arr[0]);
+    // long long arr[] = {12, 11, 13, 5, 6, 7};
+    // long long n = sizeof(arr)/sizeof(arr[0]);
     // heapSort(arr, n);
-    // printArray(arr, n);
+    // prlong longArray(arr, n);
 
     // quickSort test
 
-    // int arr1[] = {10, 7, 8, 9, 1, 5};
-    // int n1 = sizeof(arr1)/sizeof(arr1[0]);
+    // long long arr1[] = {10, 7, 8, 9, 1, 5};
+    // long long n1 = sizeof(arr1)/sizeof(arr1[0]);
     // quickSort(arr1, 0, n1-1);
-    // printArray(arr1, n1);
+    // prlong longArray(arr1, n1);
     
     
     
@@ -52,8 +52,8 @@ int main(){
 //as the pivot and places all smaller elements to the left
 //of the pivot and all larger elements to the right of the
 //pivot
-int partition(int arr[], int l, int h){
-    int pivot = arr[h]; //we always pivot on the last element
+int partition(long long arr[], int l, int h){
+    long long pivot = arr[h]; //we always pivot on the last element
     int i = l-1; //needed because we preincrement i to make
                     //make the algorithm shorter and get 
                     //rid of extraneous if-statement
@@ -74,10 +74,10 @@ int partition(int arr[], int l, int h){
     return i+1;
 }
 
-//basically quickSort just passes the last array element into
+//basically quickSort just passes the last array element long longo
 //the partition function, and resorts the list before and
 //after partitioning every time
-void quickSort(int arr[], int l, int h){
+void quickSort(long long arr[], int l, int h){
     if(l < h){
         int partitionIndex = partition(arr,l,h);
         quickSort(arr,l,partitionIndex-1);
@@ -88,16 +88,16 @@ void quickSort(int arr[], int l, int h){
 //heapsort basically just heapifies all subtrees of the array
 //by taking one element at a time, reinserting as the root and 
 //re-heapifying the array
-void heapSort(int arr[], int n){
+void heapSort(long long arr[], int n){
     //build heap
-    for(int i=n/2-1; i >= 0; i--){
+    for(long long i=n/2-1; i >= 0; i--){
         heapify(arr,n,i);
     }
 
     //swap the root with the last element of the heap 
     //and re-heapify for every element
     //to make sure heap is sorted
-    for(int i=n-1; i>=0; i--){
+    for(long long i=n-1; i>=0; i--){
         swap(arr[0],arr[i]); //swap root for last element
         heapify(arr,i,0); //reheapify
     }
@@ -105,10 +105,10 @@ void heapSort(int arr[], int n){
 
 //heapify an array, i.e. find largest element and 
 //add it as the root node
-void heapify(int arr[], int n, int i){
-    int l = i;    //initialize largest (l) element in subheap as root
-    int leftChild = 2*i+1;  //left child is its parents position*2 + 1
-    int rightChild = 2*i+2; //right child is its parents position*2 + 2
+void heapify(long long arr[], long long n, long long i){
+    long long l = i;    //initialize largest (l) element in subheap as root
+    long long leftChild = 2*i+1;  //left child is its parents position*2 + 1
+    long long rightChild = 2*i+2; //right child is its parents position*2 + 2
 
     //finding largest element 
     //check if left child is greater than root
@@ -133,16 +133,16 @@ void heapify(int arr[], int n, int i){
 }
 
 //swap two elements of an array
-void swap(int e1, int e2){
-    int temp = e1;
+void swap(long long e1, long long e2){
+    long long temp = e1;
     e1 = e2;
     e2 = temp;
 }
 
 
-//prints specified array
-void printArray(int* arr, int n){
-    for(int i = 0; i < n; i++){
+//prlong longs specified array
+void printArray(long long* arr, long long n){
+    for(long long i = 0; i < n; i++){
         if(i == 0){
             cout << "[" << arr[i] << ", ";
         }else if(i != n-1){
@@ -154,20 +154,20 @@ void printArray(int* arr, int n){
     cout << "]" << endl;
 }
 
-//returns a random array containing 5000 integers
+//returns a random array containing 5000 long longegers
 //between zero and one million
-int* initRandomArray(){
-    int* arr = new int[size];
-    for(int i = 0; i < size; i++){
-        arr[i] = (int)(rand()%1000000);
+long long* initRandomArray(){
+    long long* arr = new long long[size];
+    for(long long i = 0; i < size; i++){
+        arr[i] = (long long)(rand()%1000000);
     }
     return arr;
 }
 
 //returns deep copy of array
-int* copyArray(int* arr){
-    int* copy = new int[size];
-    for(int i = 0; i < size; i++){
+long long* copyArray(long long* arr){
+    long long* copy = new long long[size];
+    for(long long i = 0; i < size; i++){
         copy[i] = arr[i];
     }
     return copy;
